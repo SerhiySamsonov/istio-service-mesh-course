818c2fb1203edc716e58ed19f425d487a20bb17c
Serhiy Samsonov <ssamsonov@lohika.com>
2021-11-20 21:38:18 +0200

    [H3] Switched frontend to v1 and also deployed a v2 to forward "developer" header (if present) to authors service

--- h3/scripts/deploy-components.sh
+++ h3/scripts/deploy-components.sh
@@ -6,3 +6,4 @@ kubectl apply -f ../k8s/deployments/author.yml
 kubectl apply -f ../k8s/deployments/author-2.yml
 kubectl apply -f ../k8s/deployments/book.yml
 kubectl apply -f ../k8s/deployments/frontend.yml
+kubectl apply -f ../k8s/deployments/frontend-2.yml
--- h3/scripts/publish-components.sh
+++ h3/scripts/publish-components.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-docker build -t sserhiys/frontend-service:1.0 ../frontend
-docker push sserhiys/frontend-service:1.0
+docker build -t sserhiys/frontend-service:2.0 ../frontend
+docker push sserhiys/frontend-service:2.0
 
-#docker build -t sserhiys/books-service:1.0 ../books
-#docker push sserhiys/books-service:1.0
-#
-#docker build -t sserhiys/authors-service:2.0 ../authors
-#docker push sserhiys/authors-service:2.0
+docker build -t sserhiys/authors-service:2.0 ../authors
+docker push sserhiys/authors-service:2.0
